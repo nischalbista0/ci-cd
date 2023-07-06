c2e3386b516ec22731ff28ccdf71df079c8c6ec3
Achyut <actimsina@gmail.com>
2023-07-06 09:22:43 +0545

    skills added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       {/* <Application /> */}
-      {/* <Skills skills={skills} /> */}
+      <Skills skills={skills} />
       {/* <Counter /> */}
       <Users />
     </div>
